f2be00fae989d1308be2e269f57e113aec1abc9e
MathisWellmann <wellmannmathis@gmail.com>
2024-02-21 00:25:39 +0100

    Extract out `statistical_moments.rs` and export it publicly.

--- src/account_tracker/mod.rs
+++ src/account_tracker/mod.rs
@@ -4,8 +4,10 @@ mod account_tracker_trait;
 mod d_ratio;
 mod full_track;
 mod no_track;
+mod statistical_moments;
 
 pub use account_tracker_trait::AccountTracker;
 pub use d_ratio::d_ratio;
 pub use full_track::{FullAccountTracker, ReturnsSource};
 pub use no_track::NoAccountTracker;
+pub use statistical_moments::*;
